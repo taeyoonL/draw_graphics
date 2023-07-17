94e6594e77aeaeb8ec8e512955999cdd592321bf
taeyoonL <tylee3715@gmail.com>
2023-07-17 13:10:48 +0900

    Initial Commit

---
+++ draw_graphics/ViewController.swift
@@ -0,0 +1,120 @@
+//
+//  ViewController.swift
+//  draw_graphics
+//
+//  Created by 이태윤 on 2023/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var image_view: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func line(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(image_view.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.move(to: CGPoint(x: 70, y: 50))
+        context.addLine(to: CGPoint(x: 270, y: 250))
+        context.strokePath()
+        
+        context.setLineWidth(4.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        context.move(to: CGPoint(x: 170, y: 200))
+        context.addLine(to: CGPoint(x: 270, y: 350))
+        context.addLine(to: CGPoint(x: 70, y: 350))
+        context.addLine(to: CGPoint(x: 170, y: 200))
+        context.strokePath()
+        image_view.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func square(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(image_view.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        context.addRect(CGRect(x: 70, y: 100, width: 200, height: 200))
+        context.strokePath()
+        image_view.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func circle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(image_view.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setLineWidth(3.0)
+        context.setStrokeColor(UIColor.gray.cgColor)
+        context.addEllipse(in: CGRect(x: 70, y: 50, width: 200, height: 200))
+        
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.black.cgColor)
+        context.addEllipse(in: CGRect(x: 70, y: 200, width: 200, height: 100))
+        context.strokePath()
+        image_view.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func arc(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(image_view.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        
+        context.move(to: CGPoint(x: 100, y: 50))
+        context.addArc(tangent1End: CGPoint(x: 250, y: 50), tangent2End: CGPoint(x: 250, y: 200), radius: CGFloat(50))
+        context.addLine(to: CGPoint(x: 250, y: 200))
+        
+        context.move(to: CGPoint(x: 100, y: 250))
+        context.addArc(tangent1End: CGPoint(x: 400, y: 250), tangent2End: CGPoint(x: 400, y: 300), radius: CGFloat(40))
+        context.addLine(to: CGPoint(x: 400, y: 300))
+        
+        context.strokePath()
+        image_view.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+    @IBAction func fill(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(image_view.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setLineWidth(3.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        context.setFillColor(UIColor.blue.cgColor)
+        
+        let rectangle = CGRect(x: 70, y: 50, width: 200, height: 100)
+        context.addRect(rectangle)
+        context.fill(rectangle)
+        context.strokePath()
+        
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.black.cgColor)
+        context.setFillColor(UIColor.green.cgColor)
+        let rectangle_1 = CGRect(x: 70, y: 200, width: 200, height: 200)
+        context.addEllipse(in: rectangle_1)
+        context.fillEllipse(in: rectangle_1)
+        context.strokePath()
+        
+        context.setLineWidth(2.4)
+        context.setStrokeColor(UIColor.blue.cgColor)
+        context.setFillColor(UIColor.black.cgColor)
+        context.move(to: CGPoint(x: 170, y: 350))
+        context.addLine(to: CGPoint(x: 270, y: 450))
+        context.addLine(to: CGPoint(x: 70, y: 450))
+        context.addLine(to: CGPoint(x: 170, y: 350))
+        context.fillPath()
+        context.strokePath()
+        
+        image_view.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
+    
+}
+
---
+++ draw_graphicsUITests/draw_graphicsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  draw_graphicsUITestsLaunchTests.swift
+//  draw_graphicsUITests
+//
+//  Created by 이태윤 on 2023/07/17.
+//
+
+import XCTest
+
+final class draw_graphicsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
